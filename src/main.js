98a54b5d45f127dbe840ccfa07b22661416b2e46
Ayush Goyal <ayush.g@greenwayappliances.com>
2023-07-21 13:05:51 +0530

    bug fix: vuetify error resolved

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuetify from 'vuetify';
-
-Vue.use(Vuetify);
-
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   render: h => h(App),
 }).$mount('#app')
