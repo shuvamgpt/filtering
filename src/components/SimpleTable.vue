53332f39f1d8b08567ae729d89fd31b88f4168de
shuvamgpt <shuvam@offsetfarm.io>
2023-07-24 10:36:34 +0530

    changes

--- src/components/SimpleTable.vue
+++ src/components/SimpleTable.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
     <div>
-      <v-app id="inspire">
+      <v-app>
         <v-card>
           <v-card-title>
             Nutrition
@@ -14,136 +13,115 @@
               hide-details
             ></v-text-field>
           </v-card-title>
-          <v-data-table :headers="headers" :items="desserts" :search="search">
+          <v-container>
+            <v-row :key="header.value" v-for="header in headers">
+              <v-col :key="header.value">
+                <template v-if="header.value === 'name'">
+                  {{ header.text }}
+                </template>
+                <template v-else>
+                  <v-row align="center">
+                    <v-col>
+                      <v-select
+                        v-if="filterOptions[header.value].length > 0"
+                        v-model="filterValues[header.value]"
+                        :items="filterOptions[header.value]"
+                        label="Filter"
+                        multiple
+                        chips
+                        small-chips
+                      ></v-select>
+                      <v-btn v-else @click="resetFilter(header.value)">Reset</v-btn>
+                    </v-col>
+                  </v-row>
+                </template>
+              </v-col>
+            </v-row>
+          </v-container>
+          <v-data-table
+            :headers="headers"
+            :items="filteredDesserts"
+            :search="search"
+            :options.sync="paginationOptions"
+            :server-items-length="totalData"
+            :footer-props="{
+                'items-per-page-options': [5, 10, 15],
+            }"
+          >
+            <template v-slot:item="{ item }">
+              <tr>
+                <td>{{ item.name }}</td>
+                <td>{{ item.calories }}</td>
+                <td>{{ item.fat }}</td>
+                <td>{{ item.carbs }}</td>
+                <td>{{ item.protein }}</td>
+                <td>{{ item.iron }}</td>
+              </tr>
+            </template>
           </v-data-table>
         </v-card>
       </v-app>
     </div>
-  </div>
-</template>
-
-<script>
-export default {
-  name: "simpleTable",
-  data() {
-    return {
-      search: "",
-      headers: [
-        {
-          text: "Dessert (100g serving)",
-          align: "start",
-          //   sortable: false,
-          value: "name",
-        },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
-        { text: "Carbs (g)", value: "carbs" },
-        { text: "Protein (g)", value: "protein" },
-        { text: "Iron (%)", value: "iron" },
-      ],
-      desserts: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: 1,
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: 1,
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: 7,
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: 8,
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: 16,
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: 0,
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: 2,
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: 45,
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: 22,
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: 6,
-        },
-        {
-          name: "Random",
-          calories: 43,
-          fat: 12,
-          carbs: 3,
-          protein: 65,
-
-          iron: 3,
-        },
-
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: 1,
-        },
-      ],
-    };
-  },
-};
-</script>
-
-<style scoped></style>
+  </template>
+  
+  <script>
+  import dessertsData from "./desserts.js";
+  
+  export default {
+    name: "simpleTable",
+    data() {
+      return {
+        search: "",
+        headers: [
+          { text: "Dessert (100g serving)", value: "name" },
+          { text: "Calories", value: "calories" },
+          { text: "Fat (g)", value: "fat" },
+          { text: "Carbs (g)", value: "carbs" },
+          { text: "Protein (g)", value: "protein" },
+          { text: "Iron (%)", value: "iron" },
+        ],
+        desserts: dessertsData,
+        filterOptions: {},
+        filterValues: {},
+        paginationOptions: {},
+        totalData: 12
+      };
+      
+    },
+    computed: {
+      filteredDesserts() {
+        const filters = this.filterValues;
+        return this.desserts.filter((dessert) => {
+          return (
+            (filters.name.length === 0 || filters.name.includes(dessert.name.toString())) &&
+            (filters.calories.length === 0 || filters.calories.includes(dessert.calories.toString())) &&
+            (filters.fat.length === 0 || filters.fat.includes(dessert.fat.toString())) &&
+            (filters.carbs.length === 0 || filters.carbs.includes(dessert.carbs.toString())) &&
+            (filters.protein.length === 0 || filters.protein.includes(dessert.protein.toString())) &&
+            (filters.iron.length === 0 || filters.iron.includes(dessert.iron.toString()))
+          );
+        });
+      },
+    },
+    created() {
+      this.extractFilterOptions();
+    },
+    methods: {
+      extractFilterOptions() {
+        for (const header of this.headers) {
+          const uniqueValues = Array.from(
+            new Set(this.desserts.map((item) => item[header.value].toString()))
+          );
+          this.$set(this.filterOptions, header.value, uniqueValues);
+          this.$set(this.filterValues, header.value, []);
+        }
+      },
+      resetFilter(column) {
+        this.filterValues[column] = [];
+      },
+    },
+  };
+  </script>
+  
+  <style scoped> </style>
+  
