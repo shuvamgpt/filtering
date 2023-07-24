2e80c7cb1a7a3dafe9f976da9f5c87b29b963959
shuvamgpt <shuvam@offsetfarm.io>
2023-07-24 13:13:35 +0530

    deleting unnecessary files

--- src/main.js
+++ src/main.js
@@ -1,17 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import 'vuetify/dist/vuetify.min.css';
-import CustomIcon1 from './assets/icons/customIcon1.svg'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import "vuetify/dist/vuetify.min.css";
 
-Vue.config.productionTip = false
-
-vuetify.icons = {
-  customIcon1: CustomIcon1, 
-};
-
+Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
